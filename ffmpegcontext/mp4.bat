c62ff86561646baa33f796e8a11b77ff2d24e117
root <root@point-sasazuka>
2024-11-09 18:33:32 +0900

    mp4 output with image
    * ffmpeg version up to current
    * add image.png
    * change mp4 option

--- ffmpegcontext/mp4.bat
+++ ffmpegcontext/mp4.bat
@@ -1,1 +1,1 @@
-@ffmpeg -y -loglevel 0 -i "%~1" "%~dpn1.mp4"
+@ffmpeg -y -loglevel 0 -loop 1 -i %systemroot%\ffmpegcontext\image.png -i "%~1" -c:v libx264 -c:a aac -b 128k -t 140 -pix_fmt yuv420p -shortest "%~dpn1.mp4"
